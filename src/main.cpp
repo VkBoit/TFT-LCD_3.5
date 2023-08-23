c2d75c68d42e26c3b9901123baaa86431e99a40f
Boit Victor <toovic@ymail.com>
2023-08-23 13:59:12 +0300

    Ceberus

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
+#include <dog.h>
+# include <font.h>
 
 
 TFT_eSPI tft = TFT_eSPI();
@@ -8,7 +10,13 @@ TFT_eSPI tft = TFT_eSPI();
 void setup() {
   tft.init();
   tft.fillScreen(TFT_BLACK);
-  
+  tft.setSwapBytes(true);
+
+  tft.pushImage(0, 0, 128, 128, dog);
+  tft.setFreeFont(&Creepster_Caps_Regular_23);
+
+  tft.drawString("Ceberus", 20, 130);
+
 }
 
 void loop() {
