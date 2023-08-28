7e80862819b2715cc6b778289037c2652f1b145a
Boit Victor <toovic@ymail.com>
2023-08-28 12:51:42 +0300

    car game with 2 buttons with debounce

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,48 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
-#include <dog.h>
-# include <font.h>
 
 
 TFT_eSPI tft = TFT_eSPI();
 
+int px = 0; // player car x- coordinate
+int py = 416;     // player car y-coordinate
+
+int carSize = 64;
+unsigned long debounceDelay = 50;
+unsigned long lastDebounceTime = 0;
+
+// Functions
+void drawPlayer(void);
+
 void setup() {
+  pinMode(34, INPUT);
+  pinMode(35, INPUT);
   tft.init();
   tft.fillScreen(TFT_BLACK);
-  tft.setSwapBytes(true);
-
-  tft.pushImage(30, 30, 128, 128, dog);
-  tft.setFreeFont(&Creepster_Caps_Regular_23);
-
-  tft.drawString("Ceberus", 20, 130);
-
+  tft.fillRect(px, py, carSize, carSize, TFT_GREEN);
 }
 
 void loop() {
+  drawPlayer();
+}
 
+void drawPlayer() {
+  if (digitalRead(34) && px < 256) {
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      tft.fillRect(px, py, carSize, carSize, TFT_BLACK);
+      px = px + 64;
+      tft.fillRect(px, py, carSize, carSize, TFT_GREEN);
+    }
+    lastDebounceTime = millis();
+  }
+
+  if (digitalRead(35) && px > 0) {
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      tft.fillRect(px, py, carSize, carSize, TFT_BLACK);
+      px = px - 64;
+      tft.fillRect(px, py, carSize, carSize, TFT_GREEN);
+    }
+    lastDebounceTime = millis();
+  } 
 }
